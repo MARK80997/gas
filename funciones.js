87a4f68c0fe6b9a9be521cf97cb20160c7160cc9
olas <merida.marcos.137@gmail.com>
2024-07-17 18:30:55 -0400

    gos-17-7-24

--- funciones.js
+++ funciones.js
@@ -7,3 +7,9 @@ function obtenerDatosHTML(nombre) {
 
 }
 
+function obtenerContactos() {
+    let hoja = SpreadsheetApp.openById('16ghgAhLTVGBf0Hb3qlUas5HMguqQF6AnEMPw2qepa_U').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
+
