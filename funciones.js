c7bd6035a902a11f0663c37e2f7962f6c3266461
olas <merida.marcos.137@gmail.com>
2024-07-24 10:19:41 -0400

    CONTIENE CRUD COMPLETO

--- funciones.js
+++ funciones.js
@@ -3,7 +3,7 @@ const HOJA = SpreadsheetApp.openById('16ghgAhLTVGBf0Hb3qlUas5HMguqQF6AnEMPw2qepa
 
 
 function doGet(datos) {
-    
+
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google');
 }
 
@@ -24,9 +24,19 @@ function obtenerContactos() {
 }
 
 
-function insertarContacto(nombre, apellidos, correo, telf) { 
+function insertarContacto(nombre, apellidos, correo, telf) {
     HOJA.appendRow([nombre, apellidos, correo, telf]);
 
 }
 
+function borraContacto(numFila) {
+    HOJA.deleteRow(numFila);
+}
 
+
+function modificarContacto(numFila, datos) {
+
+    let celdas = HOJA.getRange('A' + numFila + ':D' + numFila);
+    celdas.setValues([[datos.nombre, datos.apellidos, datos.correo, datos.telf]]);
+
+}
